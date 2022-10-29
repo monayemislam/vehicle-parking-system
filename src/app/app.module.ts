f62b8bc31fb4313ef624417fb8b0ba60c31a35aa
Monayem Islam <monayemislam@gmail.com>
2022-10-29 23:26:58 +0600

    Authentication (login) service has done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { HeaderComponent } from './layout/header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -16,7 +18,10 @@ import { HeaderComponent } from './layout/header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthenticationModule
+    AuthenticationModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/authentication/auth-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { baseUrl } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  constructor(private htttp:HttpClient) { }
+
+  public login(data:any):Observable<any>{
+    return this.htttp.post(`${baseUrl}login`,data);
+    // return this.htttp.post('http://127.0.0.1:8000/api/login',data);
+    
+  }
+}
