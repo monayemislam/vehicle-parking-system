f7d4f5a463a8bc4a31b55073af84c9cc8c6019ec
Monayem Islam <monayem.dev@gmail.com>
2022-12-04 00:36:06 +0600

    Data received from user form. Now need to send it on db

--- src/app/requests/new-requests/new-requests.component.ts
+++ src/app/requests/new-requests/new-requests.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { NearestParkingSpacesService } from 'src/app/services/nearest-parking-spaces.service';
 import { PaymentMethodService } from 'src/app/services/payment-method.service';
 
@@ -11,13 +12,17 @@ export class NewRequestsComponent implements OnInit {
 
   paymentMethodTypeList = [];
   nearestSpacesList = [];
+  formGroup!: FormGroup;
+  newParkingRequestData:any = [];
+  user:any = JSON.parse(localStorage.getItem('user')!);
 
 
-  constructor(private paymentMethod:PaymentMethodService, private nearestSpaces:NearestParkingSpacesService) { }
+  constructor(private paymentMethod:PaymentMethodService, private nearestSpaces:NearestParkingSpacesService, private fb:FormBuilder) { }
 
   ngOnInit(): void {
     this.paymentMethodType();
     this.nearestSpacesForParking();
+    this.isFormValid()
   }
 
   paymentMethodType(){
@@ -41,5 +46,35 @@ export class NewRequestsComponent implements OnInit {
     };
   }
 
+//Submit Form Data for creating new parking space requests
+isFormValid()
+  {
+    this.formGroup = new FormGroup({
+      available_space_id: new FormControl('',[Validators.required]),
+      payment_method_id: new FormControl('',[Validators.required]),
+      start_date: new FormControl('',[Validators.required]),
+      start_time: new FormControl('',[Validators.required]),
+      end_date: new FormControl('',[Validators.required]),
+      end_time: new FormControl('',[Validators.required])
+    })
+  }
+
+createNewRequestSubmit(){
+    
+    this.newParkingRequestData = {
+      'available_space_id' : this.formGroup.value.available_space_id,
+      'payment_method_id' : JSON.parse(this.formGroup.value.payment_method_id),
+      'requestor_id' :  this.user.id,
+      'requestor_latitude' : localStorage.getItem('lat')!,
+      'requestor_longitude' : localStorage.getItem('lng')!,
+      'start_date' : this.formGroup.value.start_date,
+      'start_time' : this.formGroup.value.start_time,
+      'end_date' : this.formGroup.value.end_date,
+      'end_time' : this.formGroup.value.end_time
+  };
+
+  console.log(this.newParkingRequestData);
+
+}
 
 }
